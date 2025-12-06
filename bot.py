32bd5d5f883b0a9b3ca51ee3216ababac9e4b82f
John Cervantes <johncervantes@protonmail.com>
2025-12-05 19:46:59 -0600

    added functionality to add the user id and timestamp in a json file

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 import discord
 from dotenv import load_dotenv
 import os
+import json
+from datetime import datetime
 
 load_dotenv()
 
@@ -11,9 +13,12 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+data = {}
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    load_data()
 
 @client.event
 async def on_message(message):
@@ -21,8 +26,25 @@ async def on_message(message):
         return
 
     if message.channel.id == int(os.getenv('UPDATE_CHANNEL_ID')):
-        if message.content.startswith('$hello'):
-            await message.add_reaction('✅')
-            await message.channel.send('Hello!')
+        save_data(message)
+
+
+def load_data():
+    global data
+    try:
+        with open('./output.json', 'r') as file:      
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {}
+    
+def save_data(message): 
+    global data     
+    ct = datetime.now()
+
+    data[str(message.author.id)] = ct.timestamp() 
+
+    with open('output.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
 
 client.run(os.getenv("BOT_TOKEN"))
