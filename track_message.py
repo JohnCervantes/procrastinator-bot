351e14bdc24abad06560fb022aa26aba4b023f41
priteshkhot <priteshkhot12@gmail.com>
2025-12-13 01:12:05 +0530

    Fixed logic to store user info

--- track_message.py
+++ track_message.py
@@ -17,9 +17,9 @@ data = {}
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    load_data()
+    # load_data()
     channel = client.get_channel(int(os.getenv('UPDATE_CHANNEL_ID')))  #send a message when bot is active for testing
-    await channel.send(f'{client.user} is online!')
+    await channel.send(f'I am here')
 
 @client.event
 async def on_message(message):
@@ -56,18 +56,19 @@ def load_data():
 def save_data(message, progress=False): 
     global data     
     ct = datetime.now()
+    user_id = str(message.author.id)
+    if user_id not in data:
+        data[user_id] = {"user name" : str(message.author.name)}
     
-    data[str(message.author.id)]["last message"] = {"time": ct.timestamp(), "content" : message.content}
-        # "user name" : str(message.author.name)
+    data[user_id]["last message"] = {"time": ct.timestamp(), "content" : message.content}
         
     if progress == True:
-        data[str(message.author.id)]["progress"] = {
+        data[user_id]["progress"] = {
             "procrastinator" : False,
             "time" : ct.timestamp(),
             "content" : message.content
         }
         
-
     with open('output.json', 'w') as file:
         json.dump(data, file, indent=4)
 
